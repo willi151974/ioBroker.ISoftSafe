01c38e7f8d6c63f4058e50fa9d9d6c2f8799873f
willi151974 <willi1974@web.de>
2020-05-08 10:24:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const utils = require('@iobroker/adapter-core');
 
 const request = require('request');
 const requestData = require('request');
+var crypto = require('crypto');
  
 
  
@@ -36,7 +37,30 @@ class ISoftSafe extends utils.Adapter {
         var TokenFrommyjudo;
         var UserData ;
 
-
+        function md5(bytes) {
+            if (typeof Buffer.from === 'function') {
+              // Modern Buffer API
+              if (Array.isArray(bytes)) {
+                bytes = Buffer.from(bytes);
+              } else if (typeof bytes === 'string') {
+                bytes = Buffer.from(bytes, 'utf8');
+              }
+            } else {
+              // Pre-v4 Buffer API
+              if (Array.isArray(bytes)) {
+                bytes = new Buffer(bytes);
+              } else if (typeof bytes === 'string') {
+                bytes = new Buffer(bytes, 'utf8');
+              }
+            }
+          
+            return crypto.createHash('md5').update(bytes).digest();
+          }
+          self.setObjectNotExists('Passwort_Hash', {
+            type: 'state',
+            common: {  name: 'Passwort_Hash' , type: 'string', role: 'text',read: true, write: false,}, native: {},
+        });
+        self.setState('Passwort_Hash'  , {val: md5(this.config.Password), ack: true});
 
         this.log.info('remote request started');
 
